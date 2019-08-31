26ec13b9746c6cb9d07cea977a47cc4d69cdaf0d
truesunrise <jsunrise999@gmail.com>
2019-08-31 14:50:02 +0900

    Uncontrolled Component: Ref로 DOM을 얻기

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
 export class AddPlayerForm extends React.Component {
-  state = {
-    value: ''
-  }
-
-  handleValueChange = (e) => {
-    this.setState({ value: e.target.value})
-  }
+  textInput = React.createRef();
 
   handleSubmit = (e) => {
     console.log('submit');
@@ -21,7 +15,8 @@ export class AddPlayerForm extends React.Component {
     console.log(form.checkValidity());
 
     // 3) 부모에게서 받은 콜백 펑션을 호출
-    this.props.addPlayer(this.state.value);
+    // this.textInput.current === document.getElementById("players") => 노드를 얻을 수 있음
+    this.props.addPlayer(this.textInput.current.value);
   }
 
   render() {
@@ -29,8 +24,7 @@ export class AddPlayerForm extends React.Component {
     return (
       <form id="form" className="form" onSubmit={this.handleSubmit} noValidate>
         <input id="players" className="input" type="text" placeholder="enter a player's name"
-          value={this.state.value}
-          onChange={this.handleValueChange}
+          ref={this.textInput}
           required />
         <input className="input" type="submit" value="Add Player"/>
       </form>
