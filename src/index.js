094744b17bf8f71d8b2f72ec513999b8bd1a816d
truesunrise <jsunrise999@gmail.com>
2019-09-07 16:56:38 +0900

    공통메뉴 작성 - bootstrap, reactstrap install

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
 import {Root} from "./pages/Root";
+import App from "./App";
+
+// from 없이 import하면 하위 컴포넌트에서 import 하더라도 글로벌적으로 적용된다.
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 ReactDOM.render(<Provider store={store}><Root /></Provider>, document.getElementById('root'));
 
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -3,11 +3,13 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Home} from "./Home";
 import {Heroes} from "./Heroes";
 import {Scoreboard} from "./Scoreboard";
+import {Menu} from "./Menu";
 
 export class Root extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Menu/>
         <p>공통메뉴</p>
         <Switch>
           <Route exact path="/" component={Home}></Route>
