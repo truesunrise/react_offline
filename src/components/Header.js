63a5eef3c4432756ed27e21ac27479eda664778a
truesunrise <jsunrise999@gmail.com>
2019-09-07 10:47:39 +0900

    todo: Header 컴포넌트에서 store의 title을 props로 내려받아서 표시하기

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 // 함수이름 : 대문자
 // 반드시 리액트 엘리먼트를 리턴
 // 함수 표현식을 쓰는 것을 권장
-export const Header = ({title, players}) => {
+ const Header = ({title, players}) => {
   // props는 readonly 속성이 있다. 부모에게서 받은 props를 자식이 바꿀 수 없다.
   // 프롭스는 위에서부터 아래로 흐른다. 중간에 프롭스를 바꾸게되면 밑으로는 흐르지만 위는 안바뀜. 프롭스는 반드시 부모컴포넌트가 바꾸어야함.
 
@@ -30,3 +30,8 @@ Header.propTypes = {
 Header.defaultProps = {
   title: 'Scoreboard'
 }
+
+// store의 title을 props로 내려받아서 화면에 표시하기
+
+
+export default Header;
